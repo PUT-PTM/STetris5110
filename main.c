d6a624786dfa9ce20af0b7314b76471b051fd0c1
Duniak <nabuchodonozordthefirst@gmail.com>
2017-05-23 09:33:27 +0200

    1
    1

--- main.c
+++ main.c
@@ -18,16 +18,15 @@ int jest;
 int utw;
 int spawned;
 }bloczek;
-
-void utwspawned(bloczek** tab)
+bloczek tab[WIDTH][LENGTH];
+void utwspawned()
 {
-	int j=0;
-for(j;j<LENGTH;j++) for(int i=0;i<WIDTH;i++) if(tab[i][j].spawned)tab[i][j].utw=1;
-j=0;
-for(j;j<LENGTH;j++) for(int i=0;i<WIDTH;i++)tab[i][j].spawned=0;
+int i,j;
+for(j=0;j<LENGTH;j++) for(i=0;i<WIDTH;i++)if(tab[i][j].spawned)tab[i][j].utw=1;
+for(j=0;j<LENGTH;j++) for(i=0;i<WIDTH;i++)tab[i][j].spawned=0;
 }
 
-void spawn(bloczek** tab){
+void spawn(){
 	tab[WIDTH/2][0].jest=1;
 	tab[WIDTH/2][1].jest=1;
 	tab[WIDTH/2-1][1].jest=1;
@@ -36,18 +35,20 @@ void spawn(bloczek** tab){
 	tab[WIDTH/2][1].spawned=1;
 	tab[WIDTH/2-1][1].spawned=1;
 	tab[WIDTH/2+1][0].spawned=1;
+
 }
 
-void wszystkowdol(int n, bloczek** tab)
+void wszystkowdol(int n)
 {
-for(int k=0;k<WIDTH;k++)
+	int i,j,k;
+for(k=0;k<WIDTH;k++)
     {
         tab[k][n].jest=0;
         tab[k][n].utw=0;
         tab[k][n].spawned=0;
         punkty+=WIDTH*4;
     }
-for(int i=n-1;i>0;i--)for(int j=0;j<WIDTH;j++){
+for(i=n-1;i>0;i--)for(j=0;j<WIDTH;j++){
         tab[j][i].jest=tab[j][i-1].jest;
         tab[j][i].utw=tab[j][i-1].utw;
         tab[j][i].spawned=tab[j][i-1].spawned;
@@ -58,39 +59,39 @@ void zakonczgre()
 
 }
 
-void print(bloczek** tab)
-{
-	 for(int j=0;j<LENGTH;j++)
-	 {
-		 for(int i=0;i<WIDTH;i++)
-	 	 {
-		 if(tab[i][j].jest==1)PCD8544_DrawPixel(i, j, PCD8544_Pixel_Set);
-	     }
-	 }
-}
+//void print()
+//{
 
-void TIM3_IRQHandler(bloczek** tab)//TIMER odpowiedzialny za "ticki" gry i "spadanie" klocków. f= 1Hz
+
+//}
+
+void TIM3_IRQHandler()//TIMER odpowiedzialny za "ticki" gry i "spadanie" klocków. f= 1Hz
 {
+
+
              if(TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
              {
-
-            	 for(int j=LENGTH-1;j>-1;j--){
-            	         for(int i=0;i<WIDTH;i++)
+            	 int i,j;
+            	 GPIO_ToggleBits(GPIOD,GPIO_Pin_15);
+            	 for(j=LENGTH-1;j>-1;j--){
+            	         for(i=0;i<WIDTH;i++)
             	         {
             	             if(tab[i][j].utw==1)continue;//Z jakiegos powodu po zmianie bool na int gdzies jest blad, dopisanie == 1 pomoglo...
             	             if(j==LENGTH-1 && tab[i][j].jest==1)
             	                 {
             	                 //Jesli bloczek jest na samym dole, utwierdzamy (nie spada)
             	                 utwspawned(tab);
+            	                 spawn();
             	                 }
             	             else if(tab[i][j+1].utw==1 && tab[i][j].jest==1 &&tab[i][j].utw==0)
             	                 {
             	                 //Jesli bloczek jest na polu i,j, a pod nim jest bloczek utwierdzony, utwierdzamy tez bloczek na pol i,j
             	                 utwspawned(tab);
+            	                 spawn();
             	                 }
 
             	         }
-            	          for(int i=0;i<WIDTH;i++)if(tab[i][j].jest==1 && tab[i][j+1].jest==0 && tab[i][j].utw==0)//Jesli bloczek jest na polu i,j i nic pod nim nic nie ma, spada nizej o 1
+            	          for(i=0;i<WIDTH;i++)if(tab[i][j].jest==1 && tab[i][j+1].jest==0 && tab[i][j].utw==0)//Jesli bloczek jest na polu i,j i nic pod nim nic nie ma, spada nizej o 1
             	                 {
             	                 tab[i][j].jest=0;
             	                 tab[i][j+1].jest=1;
@@ -99,24 +100,39 @@ void TIM3_IRQHandler(bloczek** tab)//TIMER odpowiedzialny za "ticki" gry i "spad
             	                 tab[i][j].spawned=0;
             	                 }
             	 }
-            	 for(int j=0;j<LENGTH;j++)
+
+            	 for(j=0;j<LENGTH;j++)
             	     {
             	         int suma=0;
-            	         for(int i=0;i<WIDTH;i++){
+            	         for(i=0;i<WIDTH;i++){
             	         suma+=tab[i][j].jest;
             	         }
-            	         if(suma==WIDTH)wszystkowdol(j,tab);
+            	         if(suma==WIDTH)wszystkowdol(j);
             	 }
-            	 for(int i=0;i<WIDTH;i++)if(tab[i][0].utw==1)zakonczgre();
+            	 for(i=0;i<WIDTH;i++)if(tab[i][0].utw==1)zakonczgre();
             	 //->Clear screen na wyswietlaczu
             	 TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
              }
 }
-void TIM2_IRQHandler(bloczek** tab)//Odpowiedzialny za wyswietlanie i update wyswietlacza (f= 100 Hz)
+void TIM2_IRQHandler()//Odpowiedzialny za wyswietlanie i update wyswietlacza (f= 100 Hz)
 {
              if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
              {
-            	    print(tab);//->Print na wyswietlaczu
+            	 GPIO_ToggleBits(GPIOD,GPIO_Pin_14);
+            	 int i,j;
+            	 	 for(j=0;j<LENGTH;j++)
+            	 	 {
+            	 		 for(i=0;i<WIDTH;i++)
+            	 	 	 {
+
+            	 		 if(tab[i][j].jest==1){
+            	 			 PCD8544_GotoXY(i,j);
+            	 			 PCD8544_DrawPixel(i, j, PCD8544_Pixel_Set);
+            	 			 PCD8544_Refresh();
+
+            	 		 }
+            	 	     }
+            	 	 }
                     TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
              }
 }
@@ -124,100 +140,120 @@ void TIM2_IRQHandler(bloczek** tab)//Odpowiedzialny za wyswietlanie i update wys
 int main(void)
 {
 	SystemInit();
-	/*
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-	TIM_TimeBaseStructure.TIM_Period = 839;
-	TIM_TimeBaseStructure.TIM_Prescaler = 999;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure.TIM_CounterMode =  TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure2;
-	TIM_TimeBaseStructure2.TIM_Period = 8399;
-	TIM_TimeBaseStructure2.TIM_Prescaler = 9999;
-	TIM_TimeBaseStructure2.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure2.TIM_CounterMode =  TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure2);
-
-	TIM_Cmd(TIM2, ENABLE);
-	TIM_Cmd(TIM3, ENABLE);
-
-	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
-	NVIC_InitTypeDef NVIC_InitStructure2;
-	NVIC_InitStructure2.NVIC_IRQChannel = TIM3_IRQn;
-	NVIC_InitStructure2.NVIC_IRQChannelPreemptionPriority = 0x00;
-	NVIC_InitStructure2.NVIC_IRQChannelSubPriority = 0x00;
-	NVIC_InitStructure2.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure2);
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-
-	NVIC_InitTypeDef NVIC_InitStructure3;
-	NVIC_InitStructure3.NVIC_IRQChannel = EXTI1_IRQn;
-	NVIC_InitStructure3.NVIC_IRQChannelPreemptionPriority = 0x00;
-	NVIC_InitStructure3.NVIC_IRQChannelSubPriority = 0x00;
-	NVIC_InitStructure3.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure3);
-
-	EXTI_InitTypeDef EXTI_InitStructure;
-	EXTI_InitStructure.EXTI_Line = EXTI_Line0;
-	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
-	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-	EXTI_Init(&EXTI_InitStructure);
-
-	EXTI_InitTypeDef EXTI_InitStructure2;
-	EXTI_InitStructure2.EXTI_Line = EXTI_Line2;
-	EXTI_InitStructure2.EXTI_Mode = EXTI_Mode_Interrupt;
-	EXTI_InitStructure2.EXTI_Trigger = EXTI_Trigger_Rising;
-	EXTI_InitStructure2.EXTI_LineCmd = ENABLE;
-	EXTI_Init(&EXTI_InitStructure2);
-
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
-
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource2);
-*/
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	GPIO_InitTypeDef LED_Config;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+
+    int i,j;
+	for(j=0;j<LENGTH;j++)
+	{
+	  for(i=0;i<15;i++)
+	  {
+	  tab[i][j].jest=0;
+	  tab[i][j].spawned=0;
+	  tab[i][j].utw=0;
+	  }
+	}
+
+	spawn();
+
+
+		TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+		TIM_TimeBaseStructure.TIM_Period = 839;
+		TIM_TimeBaseStructure.TIM_Prescaler = 999;
+		TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+		TIM_TimeBaseStructure.TIM_CounterMode =  TIM_CounterMode_Up;
+		TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+
+		TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure2;
+		TIM_TimeBaseStructure2.TIM_Period = 8399;
+		TIM_TimeBaseStructure2.TIM_Prescaler = 9999;
+		TIM_TimeBaseStructure2.TIM_ClockDivision = TIM_CKD_DIV1;
+		TIM_TimeBaseStructure2.TIM_CounterMode =  TIM_CounterMode_Up;
+		TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure2);
+
+
+
+		TIM_Cmd(TIM2, ENABLE);
+		TIM_Cmd(TIM3, ENABLE);
+
+
+
+
+
+
+
+
+
+			NVIC_InitTypeDef NVIC_InitStructure;
+			NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+			NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
+			NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
+			NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+			NVIC_Init(&NVIC_InitStructure);
+			TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+			TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+			NVIC_InitTypeDef NVIC_InitStructure2;
+			NVIC_InitStructure2.NVIC_IRQChannel = TIM3_IRQn;
+			NVIC_InitStructure2.NVIC_IRQChannelPreemptionPriority = 0x00;
+			NVIC_InitStructure2.NVIC_IRQChannelSubPriority = 0x00;
+			NVIC_InitStructure2.NVIC_IRQChannelCmd = ENABLE;
+			NVIC_Init(&NVIC_InitStructure2);
+			TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+			TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+
+			/*
+			NVIC_InitTypeDef NVIC_InitStructure3;
+			NVIC_InitStructure3.NVIC_IRQChannel = EXTI1_IRQn;
+			NVIC_InitStructure3.NVIC_IRQChannelPreemptionPriority = 0x00;
+			NVIC_InitStructure3.NVIC_IRQChannelSubPriority = 0x00;
+			NVIC_InitStructure3.NVIC_IRQChannelCmd = ENABLE;
+			NVIC_Init(&NVIC_InitStructure3);
+
+
+			EXTI_InitTypeDef EXTI_InitStructure;
+			EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+			EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+			EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+			EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+			EXTI_Init(&EXTI_InitStructure);
+
+			EXTI_InitTypeDef EXTI_InitStructure2;
+			EXTI_InitStructure2.EXTI_Line = EXTI_Line2;
+			EXTI_InitStructure2.EXTI_Mode = EXTI_Mode_Interrupt;
+			EXTI_InitStructure2.EXTI_Trigger = EXTI_Trigger_Rising;
+			EXTI_InitStructure2.EXTI_LineCmd = ENABLE;
+			EXTI_Init(&EXTI_InitStructure2);
+
+			SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+
+			SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource2);
+
+		*/
+
+		GPIO_InitTypeDef LED_Config;
 		LED_Config.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
 		LED_Config.GPIO_Mode = GPIO_Mode_OUT;
 		LED_Config.GPIO_OType = GPIO_OType_PP;
 		LED_Config.GPIO_Speed = GPIO_Speed_25MHz;
 		LED_Config.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &LED_Config);
+	    GPIO_Init(GPIOD, &LED_Config);
 
 
 	 	PCD8544_Init(0x38);
-	    PCD8544_Init(0x38);
 
-	     //Go to x=14, y=3 position
-	     PCD8544_GotoXY(14, 3);
 
-	     //Print data with Pixel Set mode and Fontsize of 5x7px
-	     PCD8544_Puts("STM32F429", PCD8544_Pixel_Clear, PCD8544_FontSize_5x7);
-	     PCD8544_GotoXY(15, 13);
-	     PCD8544_Puts("Discovery", PCD8544_Pixel_Set, PCD8544_FontSize_5x7);
-	     PCD8544_GotoXY(30, 26);
-	     PCD8544_Puts("2014", PCD8544_Pixel_Clear, PCD8544_FontSize_5x7);
 
-	     PCD8544_GotoXY(45, 42);
-	     //Put string with Pixel set mode and Fontsize of 3x5
-	     PCD8544_Puts("majerle.eu", PCD8544_Pixel_Set, PCD8544_FontSize_3x5);
 
-	     //Display data on LCD
-	     PCD8544_Refresh();
-	    GPIO_SetBits(GPIOD,GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+
+
+
+
 
 
 
